98404b23d54461b4192c63815184331ebe96c786
Yesika Castillo <ylcastillo29@hotmail.com>
2020-03-13 21:16:26 -0500

    tienda de ropa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Producto from './components/Producto'
+import Carrito from './components/Carrito'
 
 function App() {
+  //crear lista de productos
+  const [productos, guardarProductos] = useState([
+    { id: 1, nombre: 'Camisa ReactJs', precio: 50 },
+    { id: 2, nombre: 'Camisa VueJs', precio: 40 },
+    { id: 3, nombre: 'Casmisa NodeJs', precio: 30 },
+    { id: 4, nombre: 'Camisa Angular', precio: 20 }
+  ])
+
+  //State para un carrito de compras
+  const [carrito, agregarProducto] = useState([])
+
+  //obtener la fecha
+  const fecha = new Date().getFullYear()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <Header titulo="Tienda Virtual" />
+
+      <h1>Lista de productos</h1>
+      {productos.map(producto => (
+        <Producto
+          key={producto.id}
+          productos={productos}
+          producto={producto}
+          carrito={carrito}
+          agregarProducto={agregarProducto}
+        />
+      ))}
+      <Carrito carrito={carrito} agregarProducto={agregarProducto} />
+      <Footer fecha={fecha} />
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Carrito.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Carrito.css'
+import Producto from './Producto'
+
+const Carrito = ({ carrito, agregarProducto }) => {
+  console.log({ carrito, agregarProducto })
+  return (
+    <div className="carrito">
+      <h2> tu carrito de compras</h2>
+
+      {carrito.length === 0 ? (
+        <p>No hay elementos en el carrito</p>
+      ) : (
+        carrito.map(producto => (
+          <Producto
+            key={producto.id}
+            producto={producto}
+            carrito={carrito}
+            agregarProducto={agregarProducto}
+          />
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Carrito
